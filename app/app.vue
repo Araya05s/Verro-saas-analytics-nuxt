c9550da996568b3417482a3ec956eeb53f5759ab
Araya05s <akhsanaraya5@gmail.com>
2026-08-07 10:23:26 +0700

    feat:updated SEO for image preview in the project link

--- app/app.vue
+++ app/app.vue
@@ -11,6 +11,7 @@ useHead({
   title: 'Veero | Front-End Saas Analytics',
   meta: [
     { name: 'description', content: 'A high-performance, responsive SaaS analytics platform featuring real-time data visualization filtering with state persistence and modular composable architecture. Built with Nuxt 4, Tailwind CSS, and Pinia.' },
+    { property: "og:image", content: "/screenshots/dashboard-preview.png" }
   ],
   style: [
     {
@@ -25,6 +26,14 @@ useHead({
       `)
     }
   ]
+})
+
+useSeoMeta({
+  title: 'Veero | Front-End Saas Analytics',
+  ogTitle: 'Veero | Front-End Saas Analytics',
+  description: 'A high-performance, responsive SaaS analytics platform featuring real-time data visualization filtering with state persistence and modular composable architecture. Built with Nuxt 4, Tailwind CSS, and Pinia.',
+  ogDescription: 'A high-performance, responsive SaaS analytics platform featuring real-time data visualization filtering with state persistence and modular composable architecture. Built with Nuxt 4, Tailwind CSS, and Pinia.',
+  ogImage: '/screenshots/dashboard-preview.png',
 })
 </script>
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
       htmlAttrs: { lang: 'en' },
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+      
       script: [
         {
           innerHTML: `
